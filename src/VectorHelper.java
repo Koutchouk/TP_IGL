ef7f9f005b13b1c6a3c37ed2830afcd7b0071fcc
Mehdi <fm_kherbache@esi.dz>
2017-10-15 22:09:19 +0100

    Ajout par Mehdi

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -9,8 +9,53 @@ public class VectorHelper {
 		myVector = tab;
 	}
 
+    //Tri d'un vecteur .. 
+	
+	 public void triBulle(int Vector[])
+     {
+     boolean inversion;
+     
+     do
+         {
+         inversion=false;
 
-	void inverserTab(){
+         for(int i=0;i<Vector.length-1;i++)
+             {
+             if(Vector[i]>Vector[i+1])
+                 {
+                 echanger(Vector,i,i+1);
+                 inversion=true;
+                 }
+             }
+          }
+     while(inversion);
+     }
+	 
+	 public void echanger(int [] Vector, int i, int j){
+			int tabj=Vector [j];
+			Vector [j]=Vector [i];
+			Vector [i]=tabj;
+		}
+	
+	//Somme de deux vecteurs .. 
+	 
+	 public int[] SommeVectors(int Vector1[],int Vector2[]) throws TailleDifferentesException{
+			
+			int VectorResult[]=new int[Vector1.length];
+			int i=0;
+			if(Vector1.length==Vector2.length){
+				while (i<Vector1.length){
+					VectorResult[i]=Vector1[i]+Vector2[i];
+					i++;
+				}
+			}
+			else throw new TailleDifferentesException();
+			return VectorResult;
+		}
+	 
+	 //Inversion des elements d'un vecteur .. 
+	 
+	 void inverserTab(){
 		int x,i=0;
 		while (i< (myVector.length / 2)){
 			x= myVector[i];
@@ -20,6 +65,8 @@ public class VectorHelper {
 		}
 	}
 	
+	//Le min et le max d'un vecteur simultanement .. 
+	 
 	void minMax(){
 		int i=1;
 		min = max = myVector[0];
@@ -30,6 +77,9 @@ public class VectorHelper {
 		}
 	}
 	
+	
+	//Appliquer une formule sur les elements d'un vecteur  .. 
+	
 	void multi2(int x){
 		x=x*2;
 	}
@@ -39,5 +89,9 @@ public class VectorHelper {
 			multi2(myVector[i]);
 		}
 	}
+	
+	
+	
+	
 	
 }
--- src/test.java
+++ src/test.java
@@ -10,6 +10,28 @@ public class test {
 		vh.minMax();
 		System.out.println(vh.min+"\n" + vh.max);
 		
+		vh.triBulle(vh.myVector);
+		int i=0;
+		while (i<vh.myVector.length){
+			System.out.print(vh.myVector[i]+" ");
+			i++;
+		}
+		 
+		System.out.println();
+		int vector2[]={0,1,2,3,4,5,6,7,8,9};
+		VectorHelper vh1=new VectorHelper(vector2);
+		int Result[]=new int[tata.length];
+		try {
+			Result=vh.SommeVectors(tata, vector2);
+			i=0;
+			while (i<vh.myVector.length){
+				System.out.print(Result[i]+" ");
+				i++;
+			}
+		} catch (TailleDifferentesException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
